61eaae87b7c0cf1d785e38aa92e08ef1c5b05042
Akbar <aanvarkhujaev@gmail.com>
2021-03-07 16:05:32 +0500

    Lesson_2

---
+++ VK_App/VK_App/LoginFormController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormController.swift
+//  VK_App
+//
+//  Created by Akbar on 3/3/21.
+//
+
+import UIKit
+
+class LoginFormController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
