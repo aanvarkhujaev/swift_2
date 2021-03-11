e34d682865503708416975204d304fda86c5592a
Akbar <aanvarkhujaev@gmail.com>
2021-03-11 19:01:57 +0500

    Lesson_3

---
+++ VK_App/VK_App/FriendTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FriendTableViewCell.swift
+//  VK_App
+//
+//  Created by Akbar on 3/10/21.
+//
+
+import UIKit
+
+class FriendTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+
+}
