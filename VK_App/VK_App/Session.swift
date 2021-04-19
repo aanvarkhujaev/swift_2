773c098ed3420c89050fe8a7361ee9ee5b8bee27
Akbar <aanvarkhujaev@gmail.com>
2021-04-19 23:38:48 +0500

    д1

---
+++ VK_App/VK_App/Session.swift
@@ -0,0 +1,19 @@
+//
+//  Session.swift
+//  VK_App
+//
+//  Created by Akbar on 14/04/21.
+//
+
+import Foundation
+
+class Session {
+    
+    static let shared = Session()
+    
+    private init() {}
+    
+    var token = "f54ef40e4e11ba85d44194bb8770bcbb069933af0c7d0e38b8dc5616c7a53e00f3c290a802d65c32b4723"
+    var userid = 7828458
+    
+}
---
+++ VK_App/VK_App/VkAuthViewController.swift
@@ -0,0 +1,71 @@
+//
+//  VkAuthViewController.swift
+//  VK_App
+//
+//  Created by Akbar on 17/04/21.
+//
+
+import UIKit
+import WebKit
+
+class VkAuthViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView! {
+        didSet {
+            webView.navigationDelegate = self
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureAuth()
+    }
+    
+    private func configureAuth() {
+        var urlComponents = URLComponents()
+                urlComponents.scheme = "https"
+                urlComponents.host = "oauth.vk.com"
+                urlComponents.path = "/authorize"
+                urlComponents.queryItems = [
+                    URLQueryItem(name: "client_id", value: "7828458"),
+                    URLQueryItem(name: "display", value: "mobile"),
+                    URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+                    URLQueryItem(name: "scope", value: "262150"),
+                    URLQueryItem(name: "response_type", value: "token"),
+                    URLQueryItem(name: "v", value: "5.68")
+                ]
+                
+                let request = URLRequest(url: urlComponents.url!)
+                
+                webView.load(request)
+    }
+
+}
+
+extension VkAuthViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        
+        guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment  else {
+            decisionHandler(.allow)
+            return
+        }
+        
+        let params = fragment
+            .components(separatedBy: "&")
+            .map { $0.components(separatedBy: "=") }
+            .reduce([String: String]()) { result, param in
+                var dict = result
+                let key = param[0]
+                let value = param[1]
+                dict[key] = value
+                return dict
+        }
+        
+        let token = params["access_token"]
+        
+        print(token)
+        
+        
+        decisionHandler(.cancel)
+    }
+}
